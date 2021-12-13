de6e2a0385894c7eade7be6d03d64da7b6219d75
shaniLevin1 <shani032@gmail.com>
2021-12-13 20:38:38 +0200

    add redister and login pages

---
+++ app/src/main/java/Activities/SupplierRegister.java
@@ -0,0 +1,14 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupplierRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_register);
+    }
+}
